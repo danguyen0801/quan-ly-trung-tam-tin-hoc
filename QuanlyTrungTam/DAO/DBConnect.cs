5c5615eb7eae2fb93a5353110c79009cf4959723
Truong Thao <thaotruonggl30@gmail.com>
2021-07-08 21:10:53 +0700

    Update DBConnect.cs

--- QuanlyTrungTam/DAO/DBConnect.cs
+++ QuanlyTrungTam/DAO/DBConnect.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Windows;
+//using System.Configuration;
+//using System.Data;
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DAO
-{
-    public class DBConnect
-    {
-        public static string CnnVal(string name)
-        {
-            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
-        }
+//namespace DAO
+//{
+//    public class DBConnect
+//    {
+//        public static string CnnVal(string name)
+//        {
+//            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+//        }
 
-        public IDbConnection CreateConnection()
-        {
-            string strConString = CnnVal("QLHSDb");
-            var conn = new SqlConnection(strConString);
-            try
-            {
-                conn.Open();
-                conn.Close();
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message,"Timed Out",
-                    MessageBoxButton.OK,MessageBoxImage.Error);
-                return new SqlConnection();
-            }
+//        public IDbConnection CreateConnection()
+//        {
+//            string strConString = CnnVal("QLHSDb");
+//            var conn = new SqlConnection(strConString);
+//            try
+//            {
+//                conn.Open();
+//                conn.Close();
+//            }
+//            catch (SqlException ex)
+//            {
+//                //MessageBox.Show(ex.Message,"Timed Out",
+//                //    MessageBoxButton.OK,MessageBoxImage.Error);
+//                return new SqlConnection();
+//            }
 
-            return new SqlConnection(strConString);
-        }
-    }
-}
+//            return new SqlConnection(strConString);
+//        }
+//    }
+//}
