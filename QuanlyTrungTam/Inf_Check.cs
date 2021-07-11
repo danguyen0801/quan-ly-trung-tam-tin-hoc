121de9e62d95206489fe4df8652c343277691809
Truong Thao <thaotruonggl30@gmail.com>
2021-07-12 00:15:49 +0700

    update merge Vi

--- QuanlyTrungTam/Inf_Check.cs
+++ QuanlyTrungTam/Inf_Check.cs
@@ -16,64 +16,7 @@ namespace QuanlyTrungTam
         public Inf_Check()
         {
             InitializeComponent();
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void xemThờiKhóaBiểuToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            Inf_Edit f = new Inf_Edit();
-            this.Hide();
-            f.ShowDialog();
-            this.Show();
-        }
-
-        private void button1_Click_2(object sender, EventArgs e)
-        {
-            dataGridView1.DataSource = PHIEUHOCPHI.XemHocPhi() ;
-        }
-
-        private void button1_Click_3(object sender, EventArgs e)
-        {
-            dataGridView1.DataSource = LICHTHI.XemLichThi();
-        }
-
-        private void button1_Click_4(object sender, EventArgs e)
-        {
-            dataGridView1.DataSource = BANGDIEM.XemDiemThi();
-        }
-
-        private void Semester_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
+            dataGridView1.DataSource = MONHOC.ThoiKhoaBieu();
         }
 
         private void Schedule_Button_Click(object sender, EventArgs e)
@@ -83,7 +26,32 @@ namespace QuanlyTrungTam
 
         private void button1_Click_5(object sender, EventArgs e)
         {
+            
+           
             this.Close();
+        }
+
+        private void Inf_Button_Click(object sender, EventArgs e)
+        {
+            Inf_Edit f = new Inf_Edit();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+        }
+
+        private void Fee_Button_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = PHIEUHOCPHI.XemHocPhi();
+        }
+
+        private void Semester_Button_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = LICHTHI.XemLichThi();
+        }
+
+        private void Scores_Button_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = BANGDIEM.XemDiemThi();
         }
     }
 }
